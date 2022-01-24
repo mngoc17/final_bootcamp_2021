d4af06a8a60ce37d5a4ae67edf11e37271aab2d2
Khanh Do <khanhdo.pmp@gmail.com>
2022-01-24 19:51:53 +0700

    Update ProductPageLocators.java

--- src/main/java/Locators/ProductPageLocators.java
+++ src/main/java/Locators/ProductPageLocators.java
@@ -1,4 +1,23 @@
 package Locators;
 
 public class ProductPageLocators {
+    public static By label_product_name(int index) {
+    	return By.xpath("(//div[@class='inventory_item'])[" + index + "]//div[@class='inventory_item_name']");
+    }
+
+    public static By label_product_desc(int index) {
+    	return By.xpath("(//div[@class='inventory_item'])[" + index + "]//div[@class='inventory_item_desc']");
+    }
+
+    public static By label_product_price(int index) {
+    	return By.xpath("(//div[@class='inventory_item'])[" + index + "]//div[@class='inventory_item_price']");
+    }
+
+     public static By button_add_to_cart(int index) {
+    	return By.xpath("(//div[@class='inventory_item'])[" + index + "]//button[text()='Add to cart']");
+    }
+
+    public static By button_remove(int index) {
+    	return By.xpath("(//div[@class='inventory_item'])[" + index + "]//button[text()='Remove']");
+    }
 }
