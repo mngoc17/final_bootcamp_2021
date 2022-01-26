5412a618dcb21050b0a25224e8d0867e82e7ec57
NguyenMinhNgoc <ngocnm171196@gmail.com>
2022-01-26 16:22:34 +0700

    Get product information in the Overview page

--- src/main/java/Pages/OverviewPage.java
+++ src/main/java/Pages/OverviewPage.java
@@ -3,7 +3,7 @@ package Pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class OverviewPage  extends BasePage{
+public class OverviewPage extends BasePage{
 
     public OverviewPage(WebDriver driver) {
         super(driver);
